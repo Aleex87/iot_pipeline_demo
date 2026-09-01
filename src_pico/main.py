73d9254576839a08232e587572f32f5b51bbe88f
Aleex87 <alessandrodanteabbate@gmail.com>
2026-09-01 14:41:35 +0200

    connect device to the brocker and live in the container

--- src_pico/main.py
+++ src_pico/main.py
@@ -2,14 +2,31 @@ from time import sleep
 from wifi import connect_wifi
 from machine import Pin 
 from dht import DHT11
+from umqtt.simple import MQTTClient
+import json
 
-sleep(.1)
+sleep(0.1)
+
+# byte rappresentation -> mosquitto needs this
+TOPIC = b"home/pico/dht11"
+
+# ipconfig from the terminal and in windows chose the ipv4 
+MQTT_BROCKER = "172.18.32.1"
 
 led = Pin(15, Pin.OUT)
 
 if connect_wifi():
     led.value(1)
+    
 dht_sensor = DHT11(Pin(3)) 
+
+def connect_mqtt():
+    client = MQTTClient(client_id="pico", server=MQTT_BROCKER, port=1883)
+    client.connect()
+    print("Connect to MQTT")
+    return client
+
+client = connect_mqtt()
 
 while True:
     sleep(5)
@@ -19,4 +36,7 @@ while True:
 
     data={"temperature":temp, "humidity":humidity}
     print(data)
-    
+    # dict -> 
+    payload = json.dumps(data)
+    client.publish(TOPIC, payload)
+    sleep(1)
